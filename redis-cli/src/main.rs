403fbbe8e87b106f48682d2d2b7d9a1b0e4294f3
mstmdev <mstmdev@gmail.com>
2023-06-21 16:45:08 +0800

    Implement the help and version options for redis-cli

--- redis-cli/src/main.rs
+++ redis-cli/src/main.rs
@@ -1,3 +1,17 @@
+mod help;
+
+use std::env;
+use std::process;
+use redis_core::version;
+
 fn main() {
-    println!("Hello, redis-cli!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        help::print_usage(0);
+    }
+    let arg1 = &args[1];
+    if arg1 == "-v" || arg1 == "--version" {
+        println!("{}", version::redis_cli_version());
+        process::exit(0);
+    }
 }
